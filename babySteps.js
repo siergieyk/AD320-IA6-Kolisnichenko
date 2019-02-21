27b97fdb6cdc2ab893ddfc236ed9c279a5d9df3c
siergieyk <siergieyk@gmail.com>
2019-02-20 21:39:19 -0800

    Update

--- babySteps.js
+++
@@ -1,8 +0,0 @@
-var totalSum = 0;
-
-for (var i = 2; i < process.argv.length; i++) {
-    totalSum += Number(process.argv[i])
-
-};
-
-console.log(totalSum);
--- myFirstIO.js
+++
@@ -1,5 +0,0 @@
-var fs = require('fs');
-
-var text = fs.readFileSync(process.argv[2], 'utf8');
-
-console.log(text.split('\n').length - 1);
